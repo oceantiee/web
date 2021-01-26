620e79d15ab7cc372396d65f5c7f73aa726181b7
oceantiee <75776015+oceantiee@users.noreply.github.com>
2021-01-26 21:58:29 +0300

    section blog

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true
+    });
+
+  });
