62cbf838816d9f06db02c99aa98afe168a918718
oceantiee <75776015+oceantiee@users.noreply.github.com>
2021-01-27 17:12:37 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
-$(function(){
+$(function () {
 
     $('.slider').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1121,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 775,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
-  });
+    $('.menu__btn').on('click', function () {
+        $('.menu__items').toggleClass('menu__items--active')
+    })
+
+});
